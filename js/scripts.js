b8f551a366c1ffad0b5879c1fc6cfe12490fbce2
Gigi Ditz and Inthrayuth Mahaphol <gigi@>
2016-03-31 08:36:16 -0700

    modified Pig Latin script to work on a single word

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,37 @@
+// Start Business Logic
+var checkVowel = function(character) {
+  if (character === "a" || character === "e" || character === "i" || character === "o" || character === "u" || character === "A" || character === "E" || character === "I" || character === "O" || character === "U"){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
 
-
+// End Business Logic
 
 //Start Interface Logic
 $(document).ready(function(){
   $("#pig-latin").submit(function(event){
     event.preventDefault();
-    var vowels = ["a", "e", "i", "o", "u"];
+
     var word = $("input#word").val();
-    var firstChar = word.charAt(0);
-    var tempWord = "";
-    var newWord = "";
+    var tempWord = word;
+    var arrayWords = [];
 
-    console.log(word);
-    console.log(firstChar);
-
-    vowels.forEach(function(check1){
-      if (firstChar === check1) {
-        newWord = word + "ay"
+    for (i = 0; i <= word.length; i += 1) {
+      if (checkVowel(tempWord.charAt(0))) {          // This line will check the first character for Vowels
+        var resultWord = tempWord.concat("ay");      // This line will add "ay" if the first character is vowel
+        {break}
       }
-      else if (firstChar !== "a" && firstChar !== "e" && firstChar !== "i" && firstChar !== "o" && firstChar !== "u"){
-        tempWord = word + firstChar;
-        console.log(tempWord);
-        tempWord = tempWord.substr(1);
-        console.log(tempWord);
-        console.log(firstChar);
+      else {                                        // This section will move the first character to the end position. Then the first character will be deleted.
+        tempWord = tempWord.concat(tempWord.charAt(0));
+        tempWord = tempWord.slice(1);
       }
-     });
+      console.log(i);
+    };
+console.log(resultWord);
 
-
-    console.log(tempWord);
-    console.log(newWord);
 
   });
 });
