5bb1dfb3200d4d2d8b26f52e70bce9f9374057f1
IM <zign.int@gmail.com>
2016-04-10 15:05:41 -0700

    add convertWord function, add split method to split words into array. This still works only for single word

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,21 @@ var checkVowel = function(character) {
   }
 };
 
+var convertWord = function(word) {
+  var tempWord = word;
+  for (i = 0; i <= word.length; i += 1) {
+    if (checkVowel(tempWord.charAt(0))) {          // This line will check the first character for Vowels
+      var resultWord = tempWord.concat("ay");      // This line will add "ay" if the first character is vowel
+      {break}
+    }
+    else {                                        // This section will move the first character to the end position. Then the first character will be deleted.
+      tempWord = tempWord.concat(tempWord.charAt(0));
+      tempWord = tempWord.slice(1);
+    }
+  };
+
+  return resultWord;
+};
 // End Business Logic
 
 //Start Interface Logic
@@ -15,23 +30,28 @@ $(document).ready(function(){
   $("#pig-latin").submit(function(event){
     event.preventDefault();
 
-    var word = $("input#word").val();
-    var tempWord = word;
-    var arrayWords = [];
-
-    for (i = 0; i <= word.length; i += 1) {
-      if (checkVowel(tempWord.charAt(0))) {          // This line will check the first character for Vowels
-        var resultWord = tempWord.concat("ay");      // This line will add "ay" if the first character is vowel
-        {break}
+    var inputWord = $("input#word").val();
+console.log(inputWord);
+console.log(typeof(inputWord))
+    if (inputWord.indexOf(" ") === -1) {
+      $("#result").show();
+      $("#output").text(convertWord(inputWord));
+    }
+    else {
+      var arrayWords = inputWord.split(" ");
+console.log(arrayWords);
+console.log(typeof(arrayWords));
+      $("#output").empty();
+      for (i = 0; i <= arrayWords.length; i += 1) {
+        $("#result").show();
+        var singleWord = arrayWords[i].toString();
+console.log(singleWord);
+console.log(typeof(singleWord));
+        $("#output").append(convertWord(singleWord));
       }
-      else {                                        // This section will move the first character to the end position. Then the first character will be deleted.
-        tempWord = tempWord.concat(tempWord.charAt(0));
-        tempWord = tempWord.slice(1);
-      }
-      console.log(i);
-    };
-console.log(resultWord);
 
+    }
 
+    $("#word").val("");
   });
 });
